b4b38070cefe7ba1c9e0436c79ec39a80e239712
sksunil0608 <sksunil0608@gmail.com>
2024-05-15 00:30:56 +0530

    fixed horizontal scrollbar

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -9,22 +9,6 @@ const swiper = new Swiper('.swiper', {
     enabled: true,
     delay: 4000,
   },
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
 });
 
 
